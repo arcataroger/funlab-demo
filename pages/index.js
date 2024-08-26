32711895b65c83fc1ee63464a1f519da3dc2eb82
Roger Tuan <r.tuan@datocms.com>
2024-08-26 12:01:29 -0700

    Cleanup

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,16 @@ import Head from 'next/head'
 import TimeAgo from "javascript-time-ago";
 import en from 'javascript-time-ago/locale/en'
 
-TimeAgo.addDefaultLocale(en);
+TimeAgo.addDefaultLocale(en); // Initializing the relative time formatter
 
-const REVALIDATION_PERIOD = 60;
+const REVALIDATION_PERIOD = 60; // How often ISR should check for new content
 
 export default function Home(props) {
-    const {data, age, fetchDate} = props;
+    const {data, age, fetchDate} = props; // Data from getStaticProps
 
-    const relativeTimeFormatter = new TimeAgo('en-US')
-    const fetchDateParsed = new Date(fetchDate);
-    const fetchDateISO = fetchDateParsed.toISOString();
+    const relativeTimeFormatter = new TimeAgo('en-US') // Used for "minutes ago" formatting
+    const fetchDateParsed = new Date(fetchDate); // Transforming from our API's response to JS date
+    const fetchDateISO = fetchDateParsed.toISOString(); // Standardizing output
 
     return (
         <div>
@@ -20,16 +20,17 @@ export default function Home(props) {
             </Head>
 
             <main>
-                <h1>
-                    Next 12 ISR Test
-                </h1>
+                <h1>DatoCMS Next 12 ISR Test for Funlab & Netlify</h1>
 
+                {/* Basic header & debug info */}
                 <p>Content age:<code> {age} seconds
                     ({relativeTimeFormatter.format(fetchDateParsed - (age * 1000), 'mini-minute')} before fetch)</code></p>
                 <p>Fetch date: <code>{fetchDateISO}</code></p>
-                <p>Revalidate set to: <code>{REVALIDATION_PERIOD}</code>s
-                </p>
+                <p>Revalidate set to: <code>{REVALIDATION_PERIOD}</code>s</p>
+
                 <h2>API request body from getStaticProps()</h2>
+
+                {/* The actual API response that should change */}
                 <pre>
                     <code>
                         {JSON.stringify(data, null, 2)}
@@ -47,9 +48,9 @@ export const getStaticProps = (async () => {
 
     //language=gql
     const query = `
-        query SimpleQuery {
+        query ExampleQuery {
             exampleModel {
-                textField
+                textField # This is just a simple string, the only thing we care about
             }
         }
     `
@@ -57,8 +58,6 @@ export const getStaticProps = (async () => {
     const response = await fetch('https://graphql.datocms.com/', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
             'Authorization': `Bearer ${process.env.DATOCMS_READ_ONLY_API_TOKEN}`,
         },
         body: JSON.stringify({query}),
@@ -66,8 +65,12 @@ export const getStaticProps = (async () => {
     const data = await response.json()
 
     return {
-        props: {data, fetchDate: response.headers.get('date'), age: response.headers.get('age')},
-        revalidate: REVALIDATION_PERIOD,
+        props: {
+            // Our data
+            data,
+            fetchDate: response.headers.get('date'),
+            age: response.headers.get('age')},
+        revalidate: REVALIDATION_PERIOD, // Next ISR
     }
 })
 
